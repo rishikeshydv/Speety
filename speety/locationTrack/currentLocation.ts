e1274e7a67e95fb0f717c87a0305e09032e41edd
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-29 15:41:07 +0545

    chore: getLocation() updated

--- speety/locationTrack/currentLocation.ts
+++ speety/locationTrack/currentLocation.ts
@@ -1,10 +1,9 @@
-const Locator = (): Promise<number[]> => {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(
-        (onSuccess) => resolve([onSuccess.coords.latitude, onSuccess.coords.longitude]),
-        (error) => reject(error)
-      );
-    });
-  };
-  
-  export { Locator };
+function getLocation() {
+  navigator.geolocation.getCurrentPosition((success) => {
+    const _latitude = success.coords.latitude;
+    const _longitude = success.coords.longitude;
+    return { _latitude, _longitude };
+  });
+}
+
+export { getLocation };
