bfc1ae19f4e246593dd2006e626dd5d2e8749d4b
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-29 16:13:22 +0545

    feat: initial pointer added

--- speety/locationTrack/currentLocation.ts
+++ speety/locationTrack/currentLocation.ts
@@ -1,8 +1,15 @@
-function getLocation() {
-  navigator.geolocation.getCurrentPosition((success) => {
-    const _latitude = success.coords.latitude;
-    const _longitude = success.coords.longitude;
-    return { _latitude, _longitude };
+async function getLocation() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        const _latitude = success.coords.latitude;
+        const _longitude = success.coords.longitude;
+        resolve([ _latitude, _longitude ]);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
   });
 }
 
