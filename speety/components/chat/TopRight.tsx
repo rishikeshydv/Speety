ac67b76d3b1006a6b894cadef741929c7e495b5e
rishikeshydv <rishikeshadh4@gmail.com>
2024-04-10 22:42:26 -0400

    feat: calender added

--- speety/components/chat/TopRight.tsx
+++ speety/components/chat/TopRight.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 //imports for video call
 import PopoverTriggerComponent from "@/components/video/PopOverTriggerComponent";
 import PopOverComponent from "@/components/video/PopOverComponent";
 import LocationTrigger from "../location/formPop/LocationTrigger";
+
+//imports for calendar
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker';
 
 interface TopRightProps {
 callerRef:any
@@ -21,6 +33,9 @@ const TopRight:React.FC<TopRightProps> = ({callerRef,receiverRef,videoOnClick,ad
   //no need to hardcode them
   var user = "Mary";
   var imageUrl = "/old-woman.png";
+
+
+  const [currentDate, setCurrentData] = useState(new Date());
   return (
     <div className="flex justify-between bg-gray-200 rounded-2xl mt-2 h-20 px-6">
     <div className="flex items-center space-x-2">
@@ -39,29 +54,38 @@ const TopRight:React.FC<TopRightProps> = ({callerRef,receiverRef,videoOnClick,ad
       </div>
     </div>
 
-    <div className="flex items-center space-x-2">
-    <PopoverTriggerComponent src="/facetime.png" _className="w-24 h-12 rounded-full" content={<PopOverComponent callerVideoRef={callerRef} receiverVideoRef={receiverRef} callerUser="Mary" receiverUser="John"/>} videoOnClick={videoOnClick}/>
+    <div className="flex items-center">
+    <PopoverTriggerComponent src="/facetime.png" _className="w-16 h-20 rounded-full" content={<PopOverComponent callerVideoRef={callerRef} receiverVideoRef={receiverRef} callerUser="Mary" receiverUser="John"/>} videoOnClick={videoOnClick}/>
 <LocationTrigger 
 src="/map.png" 
-_className="w-12 h-12 rounded-full ml-3" 
+_className="w-18 h-20 rounded-full" 
 senderLoc={senderLoc}
 receiverLoc={receiverLoc}/>
 
-      <button>
-        <img
-          src="/about.png"
+{/* Calender Element */}
+<Popover>
+  <PopoverTrigger>
+  <img
+          src="/calendar-icon.webp"
           alt="Image description"
-          className="w-26 h-12 rounded-full ml-6"
+          className="w-26 h-14 rounded-full"
         />
-      </button>
+  </PopoverTrigger>
+  <PopoverContent style={{width:600,height:500}} className="mr-10 mt-6 bg-gray-300 rounded-3xl">
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <StaticDateTimePicker orientation="landscape" className="w-full h-full bg-gray-300 rounded-3xl py-4 px-4"/>
+    </LocalizationProvider>
+  </PopoverContent>
+</Popover>
 
+{/* 
       <a href="/buy">
         <img
           src="/cross.png"
           alt="Image description"
           className="w-26 h-12 rounded-full ml-6"
         />
-      </a>
+      </a> */}
     </div>
   </div>
 
