0c7cc7461961f1342ee4101061f7b5f198c44383
rishikeshydv <rishikeshadh4@gmail.com>
2024-05-18 23:05:46 -0400

    feat: cache feature added

---
+++ speety/cacheMemory/PresentListingCache.ts
@@ -0,0 +1,58 @@
+interface Property {
+  address: string;
+  apartment: string;
+  city: string;
+  state: string;
+  zip: string;
+  price: string;
+  beds: string;
+  baths: string;
+  houseType: string;
+  transactionType: string;
+  listedBy: string;
+  brokerId: string;
+  imageUrl: string[];
+  videoUrl: string[];
+  date: string;
+  sqft: string;
+  lotSize: string;
+  yearBuilt: string;
+  description: string;
+  parkingSpace: string;
+  amenities: string;
+}
+
+interface CacheEntry {
+    property: Property;
+    expiry: number;
+}
+
+class PresentListingCache {
+    private PLC: Map<string, CacheEntry>;
+    private lifetime: number;
+
+    constructor(lifetime: number) {
+        this.PLC = new Map<string, CacheEntry>();
+        this.lifetime = lifetime;
+    }
+
+    set(key: string, value: Property) {
+        const expiry = Date.now() + this.lifetime;
+        this.PLC.set(key, { property: value, expiry: expiry });
+    }
+
+    get(key: string):Property|null {
+        const entry = this.PLC.get(key);
+        if (!entry){
+            return null;
+        }
+        if (Date.now() > entry.expiry) {
+            this.PLC.delete(key);
+            return null;
+          }
+        return entry.property;
+
+    }
+}
+
+export default PresentListingCache;
