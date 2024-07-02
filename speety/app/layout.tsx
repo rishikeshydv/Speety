66916b364f51ab1869d8e993e5493a95405b9165
yaman1337 <yaman.sarabariya@gmail.com>
2024-07-03 01:06:28 +0545

    create new navbar

--- speety/app/layout.tsx
+++ speety/app/layout.tsx
@@ -1,29 +1,32 @@
-import type { Metadata, Viewport } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Navbar } from "@/components/navigation";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Scail: Property, Investment, and More',
-  description: 'Where Real Estate Flourish',
-  manifest:"/manifest.json",
-  icons:{
-    icon:"/scail.ico",
-  }
-}
+  title: "Scail: Property, Investment, and More",
+  description: "Where Real Estate Flourish",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/scail.ico",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
-        <head>
+      <head>
         <link rel="icon" href="/scail.ico" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
